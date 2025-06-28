a08e386daa38054e580017c3258fcbed937e69f8
SSLOnlify <AbNassif99@gmail.com>
2025-06-28 19:48:11 +0300

    checking in again

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,9 @@
-import {useState} from "react";
-
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  function handleAdd(){
-    setCount("OWWWaaW TOO BAD");
-  }
-  function handleSub(){
-    setCount(count - 1);
-  }
   return (
-    <div className="App">
-      <div className="box">
-        <p>{count}</p>
-        <button onClick={handleSub} className='sub'>Subtract</button>
-        <button onClick={handleAdd} className='add'>Add</button>
-      </div>
+    <div>
+      <h1>Helloooas daosi jdas djas </h1>
     </div>
   )
 }
 
 export default App
-
